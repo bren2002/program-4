d0afb0eb3aec9e15fe635e34600bd588dc735aa7
bren2002 <161561650+bren2002@users.noreply.github.com>
2024-04-30 22:41:29 -0700

    Update HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -5,61 +5,59 @@
 6 */
 
 #include "HashTable.h"
+#include "LinkedList.cpp" 
 #include <stdexcept>
 
-using namespace HashTableNS;
-using namespace LinkedListNS;
+namespace HashTableNS {
 
-// Constructor for HashTable
-HashTable::HashTable() {
-    for (int i = 0; i < SIZE; i++) {
-        hashArray[i] = nullptr;
-    }
-}
-
-// Destructor for HashTable
-HashTable::~HashTable() {
-    // Free all linked lists
-    for (int i = 0; i < SIZE; i++) {
-        while (hashArray[i]) {
-            deleteFirstNode(hashArray[i]);
+    // Constructor: Initialize empty hash table
+    HashTable::HashTable() {
+        for (int i = 0; i < SIZE; i++) {
+            hashArray[i] = nullptr;
         }
     }
-}
 
-// Check if the target is in the hash table
-bool HashTable::containsString(string target) const {
-    int hashValue = computeHash(target);
-    Node<string>* current = hashArray[hashValue];
-    while (current) {
-        if (current's data == target) {
-            return true;  // Found
+    // Destructor: Clean up memory
+    HashTable::~HashTable() {
+        for (int i = 0; i < SIZE; i++) {
+            Node<string>* current = hashArray[i];
+            while (current != nullptr) {
+                Node<string>* next = current->getLink();
+                delete current;
+                current = next;
+            }
         }
-        current = current's link;
     }
-    return false;  // Not found
-}
 
-// Add a new string to the hash table
-void HashTable::put(string s) {
-    int hashValue = computeHash(s);
-    Node<string>* current = hashArray[hashValue];
-    if (!current) {
-        hashArray[hashValue] = new Node<string>(s, nullptr);
-    } else {
-        while (current's link) {
-            current = current's link;
+    // Compute hash index for a given string
+    int HashTable::computeHash(string s) {
+        int hash = 0;
+        for (char c : s) {
+            hash = (hash * 31 + c) % SIZE; // Simple hash function
         }
-        current's setLink(new Node<string>(s, nullptr));
+        return hash;
     }
-}
 
-// Compute hash value for the string
-int HashTable::computeHash(string s) {
-    int hashValue = 0;
-    for (char c : s) {
-        hashValue = (hashValue * 31 + c) % SIZE;  // A simple hash function
+    // Check if the hash table contains a specific string
+    bool HashTable::containsString(string target) const {
+        int index = computeHash(target);
+        Node<string>* current = hashArray[index];
+        while (current != nullptr) {
+            if (current->getData() == target) {
+                return true;
+            }
+            current = current->getLink();
+        }
+        return false;
     }
-    return hashValue;
-}
+
+    // Add a new string to the hash table
+    void HashTable::put(string s) {
+        int index = computeHash(s);
+        if (!containsString(s)) {
+            LinkedListNS::headInsert(hashArray[index], s);
+        }
+    }
+
+} // namespace HashTableNS
 
