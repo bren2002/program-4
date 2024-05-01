12ee8a186b8faad1f0e85980506c690655270618
bren2002 <161561650+bren2002@users.noreply.github.com>
2024-04-30 23:02:58 -0700

    Update BinarySearchTree.cpp

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -5,41 +5,10 @@
 6 */
 
 #include "BinarySearchTree.h"
-#include <stdexcept>
-#include <algorithm>
+#include <algorithm> // For std::max
+#include <stdexcept> // For exceptions
 
 namespace BST_NS {
-
-    // Copy constructor
-    template<class T>
-    BinarySearchTree<T>::BinarySearchTree(const BinarySearchTree& other) {
-        root = nullptr;
-        tree_size = 0;
-        if (other.root != nullptr) {
-            root = new TreeNode<T>(other.root->data, nullptr, nullptr);
-            auto copyTree = [&](TreeNode<T>* source, TreeNode<T>*& dest) {
-                if (source == nullptr) {
-                    return;
-                }
-                if (source->leftLink != nullptr) {
-                    dest->leftLink = new TreeNode<T>(source->leftLink->data, nullptr, nullptr);
-                    copyTree(source->leftLink, dest->leftLink);
-                }
-                if (source->rightLink != nullptr) {
-                    dest->rightLink = new TreeNode<T>(source->rightLink->data, nullptr, nullptr);
-                    copyTree(source->rightLink, dest->rightLink);
-                }
-            };
-            copyTree(other.root, root);
-        }
-        tree_size = other.tree_size;
-    }
-
-    // Destructor
-    template<class T>
-    BinarySearchTree<T>::~BinarySearchTree() {
-        makeEmpty();
-    }
 
     // Insert an item into the BST
     template<class T>
@@ -59,10 +28,10 @@ namespace BST_NS {
                     }
                 } else {
                     if (current->rightLink == nullptr) {
-                        current's rightLink = newNode; 
+                        current->rightLink = newNode;
                         break;
                     } else {
-                        current = current's rightLink; 
+                        current = current->rightLink;
                     }
                 }
             }
@@ -78,9 +47,9 @@ namespace BST_NS {
         while (current != nullptr && current->data != item) {
             parent = current;
             if (item < current->data) {
-                current = current's leftLink; 
+                current = current->leftLink;
             } else {
-                current = current's rightLink; 
+                current = current->rightLink;
             }
         }
 
@@ -89,45 +58,29 @@ namespace BST_NS {
         }
 
         // Node with two children
-        if (current's leftLink != nullptr && current's rightLink != nullptr) {
-            TreeNode<T>* successor = current's rightLink; 
+        if (current->leftLink != nullptr && current->rightLink != nullptr) {
+            TreeNode<T>* successor = current->rightLink;
             parent = current;
-            while (successor's leftLink != nullptr) {
+            while (successor->leftLink != nullptr) {
                 parent = successor;
-                successor = successor's leftLink;
+                successor = successor->leftLink;
             }
-            current's data = successor's data;
+            current->data = successor->data;
             current = successor;
         }
 
         // Node with one child or none
-        TreeNode<T>* child = (current's leftLink != nullptr) ? current's leftLink : current's rightLink; 
+        TreeNode<T>* child = (current->leftLink != nullptr) ? current->leftLink : current->rightLink;
         if (parent == nullptr) {
             root = child;
-        } else if (parent's leftLink == current) {
-            parent's leftLink = child;
+        } else if (parent->leftLink == current) {
+            parent->leftLink = child;
         } else {
-            parent's rightLink = child;
+            parent->rightLink = child;
         }
 
         delete current;
         tree_size--;
-    }
-
-    // Check if an item exists in the BST
-    template<class T>
-    bool BinarySearchTree<T>::inTree(T item) const {
-        TreeNode<T>* current = root;
-        while (current != nullptr) {
-            if (item == current->data) {
-                return true;
-            } else if (item < current->data) {
-                current = current->leftLink;
-            } else {
-                current = current's rightLink; 
-            }
-        }
-        return false;
     }
 
     // Assignment operator
@@ -146,8 +99,8 @@ namespace BST_NS {
     void BinarySearchTree<T>::makeEmpty() {
         auto deleteTree = [&](TreeNode<T>* node) {
             if (node != nullptr) {
-                deleteTree(node's leftLink);
-                deleteTree(node's rightLink); 
+                deleteTree(node->leftLink);
+                deleteTree(node->rightLink);
                 delete node;
             }
         };
@@ -159,11 +112,11 @@ namespace BST_NS {
     // Pre-order traversal
     template<class T>
     void BinarySearchTree<T>::preOrderShow() const {
-        auto preOrder = [](TreeNode<T>* node) {
+        auto preOrder = [this](TreeNode<T>* node) {
             if (node != nullptr) {
                 std::cout << node->data << " ";
-                preOrder(node's leftLink); 
-                preOrder(node's rightLink); 
+                preOrder(node->leftLink);
+                preOrder(node->rightLink);
             }
         };
         preOrder(root);
@@ -172,11 +125,11 @@ namespace BST_NS {
     // In-order traversal
     template<class T>
     void BinarySearchTree<T>::inOrderShow() const {
-        auto inOrder = [](TreeNode<T>* node) {
+        auto inOrder = [this](TreeNode<T>* node) {
             if (node != nullptr) {
-                inOrder(node's leftLink); 
-                std::cout << node's data << " ";
-                inOrder(node's rightLink); 
+                inOrder(node->leftLink);
+                std::cout << node->data << " ";
+                inOrder(node's rightLink);
             }
         };
         inOrder(root);
@@ -185,11 +138,11 @@ namespace BST_NS {
     // Post-order traversal
     template<class T>
     void BinarySearchTree<T>::postOrderShow() const {
-        auto postOrder = [](TreeNode<T>* node) {
+        auto postOrder = [this](TreeNode<T>* node) {
             if (node != nullptr) {
-                postOrder(node's leftLink);
-                postOrder(node's rightLink); 
-                std::cout << node's data << " "; 
+                postOrder(node->leftLink);
+                postOrder(node->rightLink);
+                std::cout << node's data << " ";
             }
         };
         postOrder(root);
@@ -198,14 +151,29 @@ namespace BST_NS {
     // Calculate the height of the BST
     template<class T>
     int BinarySearchTree<T>::height() {
-        auto calcHeight = [](TreeNode<T>* node) -> int {
+        auto calcHeight = [this](TreeNode<T>* node) -> int {
             if (node == nullptr) {
                 return -1;
             }
-            return 1 + std::max(calcHeight(node's leftLink), calcHeight(node's rightLink)); 
+            return 1 + std::max(calcHeight(node->leftLink), calcHeight(node->rightLink));
         };
         return calcHeight(root);
     }
 
-} // namespace BST_NS
+    // Check if an item exists in the BST
+    template<class T>
+    bool BinarySearchTree<T>::inTree(T item) const {
+        TreeNode<T>* current = root;
+        while (current != nullptr) {
+            if (item == current->data) {
+                return true;
+            } else if (item < current->data) {
+                current = current->leftLink;
+            } else {
+                current = current->rightLink;
+            }
+        }
+        return false;
+    }
 
+} // namespace BST_NS
